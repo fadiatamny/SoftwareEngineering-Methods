5ac92c849d8f315e79655f63b9f1eceb2eb17dd9
Fadi AT <fadiatamny@gmail.com>
2020-04-18 20:07:35 +0300

    updated make and made the main horrible to look at :shit:

--- homeworks/hw1/Code/main.cpp
+++ homeworks/hw1/Code/main.cpp
@@ -17,14 +17,17 @@ int main(int argc, char *argv[])
         throw new Exception("couldnt fetch data");
 
     std::vector<std::string> options;
-    options.push_back("op 1");
-    options.push_back("op 2");
-    options.push_back("op 3");
+    options.push_back("LOOK");
+    options.push_back("I");
+    options.push_back("AM");
+    options.push_back("A");
+    options.push_back("CHECK");
+    options.push_back("BOX");
 
     auto list = std::vector<std::shared_ptr<Component>>();
-    list.push_back(std::shared_ptr<Label>(new Label("Some label", 0, 0, FOREGROUND_GREEN | BACKGROUND_BLACK)));
-    list.push_back(std::shared_ptr<TextBox>(new TextBox(10, 100, {0, 1}, FOREGROUND_WHITE | BACKGROUND_RED)));
-    list.push_back(std::shared_ptr<CheckBox>(new CheckBox(options, {0, 15}, FOREGROUND_WHITE | BACKGROUND_RED)));
+    list.push_back(std::shared_ptr<Label>(new Label("Some label", 0, 0, FOREGROUND_GREEN | BACKGROUND_BLACK | FOREGROUND_INTENSITY | BACKGROUND_INTENSITY)));
+    list.push_back(std::shared_ptr<TextBox>(new TextBox(10, 100, {0, 1}, FOREGROUND_WHITE | BACKGROUND_RED | BACKGROUND_INTENSITY)));
+    list.push_back(std::shared_ptr<CheckBox>(new CheckBox(options, {0, 15}, FOREGROUND_BLUE | FOREGROUND_INTENSITY | BACKGROUND_GREEN | BACKGROUND_INTENSITY)));
 
     system("cls");
 
@@ -50,6 +53,12 @@ int main(int argc, char *argv[])
         ++index;
     }
 
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0, 25});
+
+    std::cout << "\n";
+    for (auto elem : std::static_pointer_cast<CheckBox>(list[2])->getSelected())
+        std::cout << elem << "\n";
+
     resetDefaults(info.wAttributes);
     return 0;
 }
@@ -60,5 +69,4 @@ void resetDefaults(WORD attr)
     CONSOLE_CURSOR_INFO cursor = {1, TRUE};
     SetConsoleCursorInfo(hStdout, &cursor);
     SetConsoleTextAttribute(hStdout, attr);
-    system("cls");
 }
