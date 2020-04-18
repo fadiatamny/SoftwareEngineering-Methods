0aeb1695da59ae866f133800a46d866d24525c4f
Fadi AT <fadiatamny@gmail.com>
2020-04-18 18:04:42 +0300

    textbox movement
    Co-Authored-By: oreitan <oreitan@users.noreply.github.com>

--- homeworks/hw1/Code/TextBox.cpp
+++ homeworks/hw1/Code/TextBox.cpp
@@ -5,8 +5,13 @@ void TextBox::draw() throw(Exception)
     if (this->handle == NULL || this->handle == INVALID_HANDLE_VALUE)
         throw Exception("Handle ERROR: no correct handle");
 
-    CONSOLE_CURSOR_INFO cursor = {1, TRUE};
-    SetConsoleCursorInfo(this->handle, &cursor);
+    CONSOLE_CURSOR_INFO cursor;
+    GetConsoleCursorInfo(this->handle, &cursor);
+    if (!cursor.bVisible)
+    {
+        cursor.bVisible = TRUE;
+        SetConsoleCursorInfo(this->handle, &cursor);
+    }
 
     SetConsoleCursorPosition(this->handle, this->currPos);
     this->outStream << (char)0xC9;
@@ -49,7 +54,7 @@ void TextBox::draw() throw(Exception)
         SetConsoleCursorPosition(this->handle, this->currPos);
     }
 
-    ++this->currPos.X;
+    this->currPos.X += 2;
     ++this->currPos.Y;
     SetConsoleCursorPosition(this->handle, this->currPos);
 }
@@ -59,13 +64,25 @@ void TextBox::handleKeyStroke(KEY_EVENT_RECORD &e) throw(Exception)
     if (this->handle == NULL || this->handle == INVALID_HANDLE_VALUE)
         throw Exception("Handle ERROR: no correct handle");
 
-    if(!e.bKeyDown) return;
+    if (!e.bKeyDown)
+        return;
 
-    switch(e.wVirtualKeyCode)
+    switch (e.wVirtualKeyCode)
     {
-        case VK_LEFT:
-            this->moveLeft();
-        case VK_RIGHT:
-            this->moveRight();
+    case VK_LEFT:
+        this->moveLeft();
+        break;
+    case VK_RIGHT:
+        this->moveRight();
+        break;
+    case VK_UP:
+        this->moveUp();
+        break;
+    case VK_DOWN:
+        this->moveDown();
+        break;
+    default:
+        this->outStream << e.uChar.AsciiChar;
+        this->moveRight();
     }
 }
--- homeworks/hw1/Code/textbox.hpp
+++ homeworks/hw1/Code/textbox.hpp
@@ -7,9 +7,9 @@ class TextBox : public Component
 {
 public:
     TextBox() : Component({0, 0}, FOREGROUND_WHITE | BACKGROUND_BLACK), width(0), height(0) {}
-    TextBox(int height, int width, SHORT x, SHORT y, DWORD color) : Component({x, y}, color), width(width), height(height), currPos({x,y}) {}
+    TextBox(int height, int width, SHORT x, SHORT y, DWORD color) : Component({x, y}, color), width(width), height(height), currPos({x, y}) {}
     TextBox(int height, int width, COORD pos, DWORD color) : Component(pos, color), width(width), height(height), currPos(pos) {}
-    TextBox(int height, int width, SHORT x, SHORT y, DWORD color, HANDLE handle) : Component({x, y}, color, handle), width(width), height(height), currPos({x,y}) {}
+    TextBox(int height, int width, SHORT x, SHORT y, DWORD color, HANDLE handle) : Component({x, y}, color, handle), width(width), height(height), currPos({x, y}) {}
     TextBox(int height, int width, COORD pos, DWORD color, HANDLE handle) : Component(pos, color, handle), width(width), height(height), currPos(pos) {}
     ~TextBox() {}
 
@@ -17,7 +17,15 @@ public:
     void handleKeyStroke(KEY_EVENT_RECORD &e) throw(Exception) override;
     inline void moveLeft()
     {
-        if (1 - this->currPos.X > 0)
+        if (this->currPos.X - 2 <= this->coordinations.X)
+        {
+            if (this->currPos.Y - 1 > this->coordinations.Y)
+            {
+                --this->currPos.Y;
+                this->currPos.X = this->width;
+            }
+        }
+        if (this->currPos.X - 2 > this->coordinations.X)
         {
             --this->currPos.X;
             SetConsoleCursorPosition(this->handle, this->currPos);
@@ -25,12 +33,40 @@ public:
     };
     inline void moveRight()
     {
-        if (1 + this->currPos.X < width)
+        if (this->currPos.X < width)
         {
             ++this->currPos.X;
+            if (this->currPos.X >= width)
+            {
+                if (this->currPos.Y < this->height)
+                {
+                    ++this->currPos.Y;
+                    this->currPos.X = this->coordinations.X + 2;
+                }
+                else
+                {
+                    --this->currPos.X;
+                }
+            }
+        }
+        SetConsoleCursorPosition(this->handle, this->currPos);
+    };
+    inline void moveUp()
+    {
+        if (this->currPos.Y - 1 > this->coordinations.Y)
+        {
+            --this->currPos.Y;
             SetConsoleCursorPosition(this->handle, this->currPos);
         }
-    };
+    }
+    inline void moveDown()
+    {
+        if (this->currPos.Y < height)
+        {
+            ++this->currPos.Y;
+        }
+        SetConsoleCursorPosition(this->handle, this->currPos);
+    }
 
 private:
     COORD currPos;
