3865b28664877bb415b292bebc55961852931153
oreitan <oreitan13@gmail.com>
2020-07-27 21:39:14 +0300

    adding cal size of Panel
    cal the panel size by the sizes and the location of the elements inside the panel

--- finalProject/source/Common/Panel.cpp
+++ finalProject/source/Common/Panel.cpp
@@ -1,26 +1,47 @@
 #include "./Panel.hpp"
 
-Panel::Panel(short x,short y, Border* border,Color textC,Color backgroundColor):
-    Control(x,y,15,15, border,textC,backgroundColor) {
-        clacWidthAndHeight();
-    }
+Panel::Panel(short x, short y, Border *border, Color textC, Color backgroundColor) : Control(x, y, 15, 15, border, textC, backgroundColor)
+{
+    clacWidthAndHeight();
+}
 
-Panel::~Panel(){}
+Panel::~Panel() {}
 
-bool Panel::addControl(Control* control) {
-    if(control != nullptr) {
+bool Panel::addControl(Control *control)
+{
+    if (control != nullptr)
+    {
         try
         {
             controls.push_back(control);
-            if(getFocus() == nullptr && control->canGetFocus()) {
+            if (getFocus() == nullptr && control->canGetFocus())
+            {
                 setFocus(*control);
             }
         }
-        catch(...)
+        catch (...)
         {
             return false;
         }
         return false;
     }
     return false;
+}
+
+void Panel::clacWidthAndHeight()
+{
+    short calWidth = 0, calHight = 0, ciX, ciY, ciWidth, ciHight;
+    for (int i = 0; i < controls.size(); ++i)
+    {
+        ciX = controls[i]->getLeft();
+        ciY = controls[i]->getTop();
+        ciHight = controls[i]->getHeight();
+        ciWidth = controls[i]->getWidth();
+        if (ciX + ciWidth > calWidth)
+            calWidth = ciX + ciWidth;
+        if(ciY +ciHight > calHight)
+            calHight = ciY+ciHight;
+    }
+    setHeight(calHight+5);
+    setWidth(calWidth+5);
 }
