973733a3984e43232ad90f3812f551f47fe38714
Annieli Siegel <duddoodler@gmail.com>
2020-07-29 12:17:26 +0300

    add numeric header
    create base for numericBox component

---
+++ finalProject/source/Controls/NumericBox.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "./Panel.hpp"
+#include "../../Common/Control.hpp"
+#include "./Button.hpp"
+#include "./Label.hpp"
+#include <string>
+
+class NumericBox : public Panel
+{
+private:
+    int value;
+    int minVal;
+    int maxVal;
+    Button subtract;
+    Button add;
+    Label currVal;
+
+public:
+    void increase();
+    void decrease();
+    int getVal();
+    void setVal(int);
+    int getMin();
+    int setMin(int);
+    int getMax();
+    int setMax(int);
+    bool setValue(int);
+    void draw(Graphics &g, int x, int y, size_t z);
+    void notify(string text);
+    NumericBox(short left, short top, int maxVal, int minVal, BorderDrawer *border, Color textColor, Color backgroundColor);
+    ~NumericBox();
+};
